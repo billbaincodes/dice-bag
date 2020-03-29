c68221073bc70660aad6fd1857af4c1ca1481487
bill <williamandrewbain@gmail.com>
2020-03-28 22:15:25 -0600

    improve roll quality colors, remove old imports from app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
 import Dice from './Dice/Dice'
-import Physics from './Dice/Physics'
-import OnClick from './Dice/OnClick'
 
 function App() {
   return (
     <div className="App">
         <Dice />
-        {/* <OnClick /> */}
-        {/* <Physics /> */}
     </div>
   );
 }
