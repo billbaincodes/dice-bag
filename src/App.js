ac06af572f2da8b67e523660ec76e291c05f19ba
bill <williamandrewbain@gmail.com>
2020-04-03 21:20:59 -0600

    remove physics comp and add header comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
 import Dice from './Components/Dice/Dice'
+import Header from './Components/Header/Header'
 
 function App() {
   return (
     <div className="App">
+        <Header />
         <Dice />
     </div>
   );
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.scss'
+
+const Header = () => {
+  return(
+    <div>
+      Become one with inner seflness
+    </div>
+  )
+}
+
+export default Header
