ed5a5328a8d03726f0fcc8be669a4e1b764f2b04
bill <williamandrewbain@gmail.com>
2020-04-23 22:13:43 -0600

    Start ability wheel

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,24 +1,17 @@
-import React from 'react'
+import React from "react";
+import "./Skills.scss";
 
 const Component = () => {
-  return(
-    <div>
-      Skill View
-
-
-      <div className='ability-block'>
-        <div>STR</div>
-        <div>DEX</div>
-        <div>CON</div>
-        <div>INT</div>
-        <div>WIS</div>
-        <div>CHAR</div>
-      </div>
-
-
-
+  return (
+    <div id="colorWheel">
+      <span class="color01">Str</span>
+      <span class="color02"></span>
+      <span class="color03"></span>
+      <span class="color04"></span>
+      <span class="color05"></span>
+      <span class="color06"></span>
     </div>
-  )
-}
+  );
+};
 
-export default Component
+export default Component;
