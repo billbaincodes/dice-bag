4958de5ff1f60157da49a8a51944b3b0ac46f087
bill <williamandrewbain@gmail.com>
2020-05-08 20:38:51 -0600

    Start nav refactor

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
 
   return (
     <div className="App">
-        {/* <Dice /> */}
-        <Skills />
+        <Dice />
+        {/* <Skills /> */}
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,73 +8,136 @@ class Header extends Component {
 
   render() {
     return (
-      <header className={this.state.darkMode ? "light" : "dark"}>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          Navbar
+        </a>
         <button
-          className={this.state.darkMode ? "light" : "dark"}
-          onClick={() => this.props.toggleLog()}
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          Roll Log
+          <span class="navbar-toggler-icon"></span>
         </button>
-        <button
-          className={this.state.darkMode ? "light" : "dark"}
-          onClick={() => this.props.colorRandomizer()}
-        >
-          Random Color!
-        </button>
-        <button
-          className={this.state.darkMode ? "light" : "dark"}
-          onClick={() => this.props.setSpeed()}
-        >
-          Faster!!
-        </button>
-        <label htmlFor="color-set">Choose Color: </label>
-        <input
-          onChange={(e) => this.props.colorSet(e)}
-          id="color-set"
-          type="color"
-        ></input>
-        <div class="dropdown">
-          <button class="dropbtn">Dropdown</button>
-          <div class="dropdown-content">
-            <button
-            className={this.state.darkMode ? "light" : "dark"}
-            onClick={() => this.props.colorSynth()}
-            >
-              s y n t h w a v e
-            </button>
-            <button
-            className={this.state.darkMode ? "light" : "dark"}
-            onClick={() => this.props.colorSynth()}
-            >
-              Rainbow
-            </button>
-            <button
-            className={this.state.darkMode ? "light" : "dark"}
-            onClick={() => this.props.colorSynth()}
-            >
-              Tron
-            </button>
-          </div>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">
+                Home <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Features
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdownMenuLink"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Dropdown link
+              </a>
+              <div
+                class="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <a class="dropdown-item" href="#">
+                  Action
+                </a>
+                <a class="dropdown-item" href="#">
+                  Another action
+                </a>
+                <a class="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </div>
+            </li>
+          </ul>
         </div>
-        <button
-          className={this.state.darkMode ? "light" : "dark"}
-          onClick={() => this.props.darkToggle()}
-        >
-          {this.state.darkMode ? "Light" : "Dark"} Mode
-        </button>
-        <button
-          className={this.state.darkMode ? "light" : "dark"}
-          onClick={() => this.props.batterySaver()}
-        >
-          Battery Saver
-        </button>
-        <button onClick={() => this.props.colorRainbow()}></button>
-        <span onClick={() => this.props.toggleSettings()}>
-          <i className="close-cog fas fa-cog"></i>
-        </span>
-      </header>
+      </nav>
     );
   }
 }
 
 export default Header;
+
+// <header className={this.state.darkMode ? "light" : "dark"}>
+// <button
+//   className={this.state.darkMode ? "light" : "dark"}
+//   onClick={() => this.props.toggleLog()}
+// >
+//   Roll Log
+// </button>
+// <button
+//   className={this.state.darkMode ? "light" : "dark"}
+//   onClick={() => this.props.colorRandomizer()}
+// >
+//   Random Color!
+// </button>
+// <button
+//   className={this.state.darkMode ? "light" : "dark"}
+//   onClick={() => this.props.setSpeed()}
+// >
+//   Faster!!
+// </button>
+// <label htmlFor="color-set">Choose Color: </label>
+// <input
+//   onChange={(e) => this.props.colorSet(e)}
+//   id="color-set"
+//   type="color"
+// ></input>
+// <div class="dropdown">
+//   <button class="dropbtn">Dropdown</button>
+//   <div class="dropdown-content">
+//     <button
+//     className={this.state.darkMode ? "light" : "dark"}
+//     onClick={() => this.props.colorSynth()}
+//     >
+//       s y n t h w a v e
+//     </button>
+//     <button
+//     className={this.state.darkMode ? "light" : "dark"}
+//     onClick={() => this.props.colorSynth()}
+//     >
+//       Rainbow
+//     </button>
+//     <button
+//     className={this.state.darkMode ? "light" : "dark"}
+//     onClick={() => this.props.colorSynth()}
+//     >
+//       Tron
+//     </button>
+//   </div>
+// </div>
+// <button
+//   className={this.state.darkMode ? "light" : "dark"}
+//   onClick={() => this.props.darkToggle()}
+// >
+//   {this.state.darkMode ? "Light" : "Dark"} Mode
+// </button>
+// <button
+//   className={this.state.darkMode ? "light" : "dark"}
+//   onClick={() => this.props.batterySaver()}
+// >
+//   Battery Saver
+// </button>
+// <button onClick={() => this.props.colorRainbow()}></button>
+// <span onClick={() => this.props.toggleSettings()}>
+//   <i className="close-cog fas fa-cog"></i>
+// </span>
+// </header>
