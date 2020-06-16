113b0580e286a515c707e3d302c9895edd903842
bill <williamandrewbain@gmail.com>
2020-06-15 21:33:35 -0600

    add material selection to header, improve styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -17,18 +17,26 @@ class Header extends Component {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
             <button onClick={() => this.props.setSpeed()}>Faster!</button>
-            <label htmlFor="color-set">Choose Color: </label>
-            <input
-              onChange={(e) => this.props.colorSet(e)}
-              id="color-set"
-              type="color"
-            ></input>
-            <NavDropdown title="Materials" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <div class='color-set d-flex'>
+              <label htmlFor="color-set">Choose Color: </label>
+              <input onChange={(e) => this.props.colorSet(e)}
+                id="color-set"
+                type="color">
+              </input>
+            </div>
+            <NavDropdown onSelect={(e) => this.props.changeMaterial(e)} title="Materials" id="basic-nav-dropdown">
+              <NavDropdown.Item href='basic'>Plastic</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2" value='wood'>Wood</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Metal</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Stars</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Special ; )&nbsp;" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Rainbow</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Synthwave</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Galaxy</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown>
